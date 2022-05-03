f005217aad02577f998f8c04acaaf27941c38b9e
mechztcc <dojathan@gmail.com>
2022-05-03 10:43:21 -0300

    feat: added image picker to open gallery and get image

--- lib/components/user_image_picker.dart
+++ lib/components/user_image_picker.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   final void Function(File image) onImagePicker;
@@ -12,11 +13,23 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
-
   File? _image;
 
-  void _pickImage() {
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
 
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+      widget.onImagePicker(_image!);
+    }
   }
 
   @override
