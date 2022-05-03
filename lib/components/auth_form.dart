4e4832637322c65c352d04371acdc29883991b48
mechztcc <dojathan@gmail.com>
2022-05-03 08:56:07 -0300

    feat: transform auth_form into a statefull component

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,20 @@
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({Key? key}) : super(key: key);
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    _formKey.currentState?.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +22,8 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.always,
           child: Column(
             children: [
               TextFormField(
@@ -30,7 +45,7 @@ class AuthForm extends StatelessWidget {
                 height: 30,
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _submit,
                   child: const Text('Entrar'),
                 ),
               ),
