73e37229cc4fed763a4129d5c8b9d5e5dcb7cc1f
mechztcc <dojathan@gmail.com>
2022-05-05 12:48:09 -0300

    feat: create component to verify if user has logged

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:chat/pages/auth_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AuthPage(),
+      home: const AuthOrAppPage(),
     );
   }
 }
---
+++ lib/pages/auth_or_app_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthOrAppPage extends StatelessWidget {
+  const AuthOrAppPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Auth or app'),
+      ),
+    );
+  }
+}
