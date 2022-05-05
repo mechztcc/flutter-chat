9ed34e97274c8ac057481fd9c407a76712fb52dd
mechztcc <dojathan@gmail.com>
2022-05-05 12:52:40 -0300

    feat: verity if user its been logged in

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,3 +1,8 @@
+import 'package:chat/models/user_model.dart';
+import 'package:chat/pages/auth_page.dart';
+import 'package:chat/pages/chat_page.dart';
+import 'package:chat/pages/loading_page.dart';
+import 'package:chat/services/auth/auth_service_mock.dart';
 import 'package:flutter/material.dart';
 
 class AuthOrAppPage extends StatelessWidget {
@@ -5,9 +10,16 @@ class AuthOrAppPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Auth or app'),
+    return Scaffold(
+      body: StreamBuilder<UserModel?>(
+        stream: AuthServiceMock().userChanges,
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingPage();
+          } else {
+            return snapshot.hasData ? const ChatPage() : const AuthPage();
+          }
+        },
       ),
     );
   }
