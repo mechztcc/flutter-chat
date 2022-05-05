aa278f6220ef8a8d5051d5f324d44556873435c4
mechztcc <dojathan@gmail.com>
2022-05-05 11:58:22 -0300

    fix: define AuthService into abstract class, its be a contract

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,3 +1,23 @@
+import 'dart:io';
 
+import 'package:chat/models/user_model.dart';
 
-class AuthService {}
+abstract class AuthService {
+  UserModel? get currentUser;
+
+  Stream<UserModel>? get userChanges; 
+
+  Future<void> signUp(
+    String name,
+    String password,
+    String email,
+    File image,
+  );
+
+  Future<void> login(
+    String email,
+    String password,
+  );
+
+  Future<void> logout();
+}
