334d996be822396ee83041ab1fad9651514ad52e
mechztcc <dojathan@gmail.com>
2022-05-05 21:10:39 -0300

    feat: add default messages

--- lib/services/chat/chat_mock_service.dart
+++ lib/services/chat/chat_mock_service.dart
@@ -6,7 +6,24 @@ import 'package:chat/services/chat/chat_service.dart';
 import 'dart:async';
 
 class ChatMockService implements ChatService {
-  static final List<ChatMessage> _msgs = [];
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+      id: '1',
+      text: 'Bom dia',
+      createdAt: DateTime.now(),
+      userId: '1',
+      userName: 'Alberto',
+      userImageURL: 'assets/images/profile.jpg',
+    ),
+    ChatMessage(
+      id: '2',
+      text: 'Bom dia',
+      createdAt: DateTime.now(),
+      userId: '2',
+      userName: 'Daniel',
+      userImageURL: 'assets/images/profile.jpg',
+    ),
+  ];
 
   static MultiStreamController<List<ChatMessage>>? _controller;
 
