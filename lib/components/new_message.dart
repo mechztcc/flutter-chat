4d278496cc516aee5c50a281b82d9fb5523394f1
mechztcc <dojathan@gmail.com>
2022-05-06 10:17:45 -0300

    feat: add controller to handle chat input

--- lib/components/new_message.dart
+++ lib/components/new_message.dart
@@ -1,19 +1,38 @@
+import 'package:chat/services/auth/auth_service.dart';
+import 'package:chat/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({Key? key}) : super(key: key);
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+
+  final TextEditingController _controller = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+
+    if(user != null) {
+      await ChatService().save(_controller.text, user);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const Expanded(
+        Expanded(
           child: TextField(
-            decoration: InputDecoration(labelText: 'Enviar mensagem'),
+            controller: _controller,
+            decoration: const InputDecoration(labelText: 'Enviar mensagem'),
           ),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: _sendMessage,
           icon: Icon(
             Icons.send,
             color: Theme.of(context).primaryColor,
