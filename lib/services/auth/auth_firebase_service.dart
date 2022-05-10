a596e807c44c08ea6969dc204707d0c96846db3e
mechztcc <dojathan@gmail.com>
2022-05-10 13:39:59 -0300

    feat: create auth firebase service

---
+++ lib/services/auth/auth_firebase_service.dart
@@ -0,0 +1,33 @@
+
+import 'package:chat/models/user_model.dart';
+import 'dart:io';
+
+import 'package:chat/services/auth/auth_service.dart';
+
+class AuthFireBaseService implements AuthService {
+  @override
+  // TODO: implement currentUser
+  UserModel? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> login(String email, String password) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp(String name, String password, String email, File? image) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement userChanges
+  Stream<UserModel?> get userChanges => throw UnimplementedError();
+}
