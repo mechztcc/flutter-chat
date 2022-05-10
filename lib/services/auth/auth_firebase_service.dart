7bf2874b7382863e69a33553cea3c481b93fb609
mechztcc <dojathan@gmail.com>
2022-05-10 13:44:33 -0300

    feat: add signup / login with firebase

--- lib/services/auth/auth_firebase_service.dart
+++ lib/services/auth/auth_firebase_service.dart
@@ -1,8 +1,8 @@
-
 import 'package:chat/models/user_model.dart';
 import 'dart:io';
 
 import 'package:chat/services/auth/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthFireBaseService implements AuthService {
   @override
@@ -16,15 +16,28 @@ class AuthFireBaseService implements AuthService {
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    FirebaseAuth.instance.signOut();
   }
 
   @override
-  Future<void> signUp(String name, String password, String email, File? image) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<void> signUp(
+    String name,
+    String password,
+    String email,
+    File? image,
+  ) async {
+    final auth = FirebaseAuth.instance;
+
+    UserCredential credencial = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    if(credencial.user != null) {
+      credencial.user?.updateDisplayName(name);
+      // credencial.user?.updatePhotoURL(photoURL);
+    }
   }
 
   @override
