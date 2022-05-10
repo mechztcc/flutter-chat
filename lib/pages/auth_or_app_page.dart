4ba6cedbb2b69fcba44d200a61a186efb83306d8
mechztcc <dojathan@gmail.com>
2022-05-10 13:33:44 -0300

    feat: init firebase connection

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -4,21 +4,35 @@ import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:chat/services/auth/auth_service_mock.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<UserModel?>(
-      stream: AuthServiceMock().userChanges,
-      builder: (ctx, snapshot) {
+    return FutureBuilder(
+      future: init(context),
+      builder: ((context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const LoadingPage();
         } else {
-          return snapshot.hasData ? const ChatPage() : const AuthPage();
+          return StreamBuilder<UserModel?>(
+            stream: AuthServiceMock().userChanges,
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingPage();
+              } else {
+                return snapshot.hasData ? const ChatPage() : const AuthPage();
+              }
+            },
+          );
         }
-      },
+      }),
     );
   }
 }
